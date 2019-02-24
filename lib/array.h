f81dfe2afabf0413eb8359a2e1f1352048e66bca
JonnyRivers <jonny.w.rivers@gmail.com>
2019-02-24 10:30:36 -0800

    Beginnings of array_iterator

--- lib/array.h
+++ lib/array.h
@@ -5,11 +5,109 @@
 namespace jwr
 {
 	template <class T, std::size_t N>
+	class _array_const_iterator
+	{
+	public:
+		using value_type = T;
+		using difference_type = ptrdiff_t;
+		using pointer = const T*;
+		using reference = const T&;
+
+		_array_const_iterator(pointer ptr) : m_ptr(ptr) {}
+
+		constexpr reference operator*() const
+		{
+			return (*m_ptr);
+		}
+
+		constexpr pointer operator->() const
+		{
+			return m_ptr;
+		}
+
+		constexpr _array_const_iterator& operator++()
+		{	// preincrement
+			++m_ptr;
+			return *this;
+		}
+
+		constexpr _array_const_iterator operator++(int)
+		{	// postincrement
+			_array_const_iterator temp = *this;
+			++m_ptr;
+			return (temp);
+		}
+
+		constexpr _array_const_iterator& operator--()
+		{	// predecrement
+			--m_ptr;
+			return *this;
+		}
+
+		constexpr _array_const_iterator operator--(int)
+		{	// postdecrement
+			_array_const_iterator temp = *this;
+			--m_ptr;
+			return (temp);
+		}
+
+	private:
+		pointer m_ptr;
+	};
+
+	template <class T, std::size_t N>
+	class _array_iterator : public _array_const_iterator<T, N>
+	{
+	public:
+		using my_base = _array_const_iterator;
+		using pointer = T*;
+		using reference = T&;
+
+		_array_iterator(pointer ptr) : my_base(ptr) {}
+
+		constexpr reference operator*() const
+		{
+			return (const_cast<reference>(*m_ptr));
+		}
+
+		constexpr pointer operator->() const
+		{
+			return const_cast<pointer>(m_ptr);
+		}
+
+		constexpr _array_iterator& operator++()
+		{	// preincrement
+			++m_ptr;
+			return *this;
+		}
+
+		constexpr _array_iterator operator++(int)
+		{	// postincrement
+			_array_iterator temp = *this;
+			++m_ptr;
+			return (temp);
+		}
+
+		constexpr _array_iterator& operator--()
+		{	// predecrement
+			--m_ptr;
+			return *this;
+		}
+
+		constexpr _array_iterator operator--(int)
+		{	// postdecrement
+			_array_iterator temp = *this;
+			--m_ptr;
+			return (temp);
+		}
+	};
+
+	template <class T, std::size_t N>
 	struct array
 	{
 	public:
-		typedef T* iterator;
-		typedef const T* const_iterator;
+		typedef _array_iterator<T, N> iterator;
+		typedef _array_const_iterator<T, N> const_iterator;
 		typedef T& reference;
 		typedef const T& const_reference;
 
@@ -67,6 +165,36 @@ namespace jwr
 		constexpr const T* data() const
 		{
 			return m_data;
+		}
+
+		constexpr iterator begin() noexcept
+		{
+			return _array_iterator(m_data);
+		}
+
+		constexpr const_iterator begin() const noexcept
+		{
+			return _array_const_iterator(m_data);
+		}
+
+		constexpr const_iterator cbegin() const noexcept
+		{
+			return _array_const_iterator(m_data);
+		}
+
+		constexpr iterator end() noexcept
+		{
+			return _array_iterator(m_data + N);
+		}
+
+		constexpr const_iterator end() const noexcept
+		{
+			return _array_const_iterator(m_data + N);
+		}
+
+		constexpr const_iterator cend() const noexcept
+		{
+			return _array_const_iterator(m_data + N);
 		}
 	private:
 		T m_data[N];
